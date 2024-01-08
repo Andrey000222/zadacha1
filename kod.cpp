0ae337e69d652e814371bd72ea7676c8d013b1bc
Andrey000222 <113039378+Andrey000222@users.noreply.github.com>
2024-01-08 23:43:01 +0300

    Add files via upload

---
+++ kod.cpp
@@ -0,0 +1,39 @@
+﻿#include "kod.h"
+Snack::Snack() {};
+Snack::~Snack(){};
+Snack::Snack(string name) {
+	snake_name = name;
+};
+SnackSlot::SnackSlot() {};
+SnackSlot::~SnackSlot() {};
+SnackSlot::SnackSlot(short max_snack) {
+	summ_snake = 0;
+	max_count_snack = max_snack;
+	slot_name = new Snack[max_snack]();
+};
+VendingMachine::VendingMachine() {};
+VendingMachine::~VendingMachine() {};
+VendingMachine::VendingMachine(short max_slot) {
+	slot_Count = max_slot;
+	slot_masiv_caunt = new SnackSlot[max_slot]();
+}
+void SnackSlot::addSnack(Snack* name_snack) {
+	if (summ_snake <= max_count_snack) {
+		slot_name[++summ_snake] = *name_snack; 
+	}
+	else
+		cout << "слот полон";
+}
+void VendingMachine::addSlot(SnackSlot* slot) {
+	if (slot_Count > 0) {
+		slot_masiv_caunt[--slot_Count] = *slot;
+	}
+	else
+		cout << "в автомате нет места для слотов";
+}
+short VendingMachine::getEmptySlotsCount() {
+	return slot_Count;
+};
+
+
+
---
+++ kod.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+const short slotCount = 10;
+class Snack  {
+public:
+	Snack();
+	~Snack();
+	Snack(string name);
+private:
+	string snake_name;
+};
+class SnackSlot   {
+public:
+	SnackSlot();
+	~SnackSlot();
+	SnackSlot(short max_slot);
+	void addSnack(Snack* k);
+private:
+	short summ_snake;
+	short max_count_snack;
+	Snack* slot_name= nullptr;
+};
+class VendingMachine {
+public:
+	VendingMachine();
+	~VendingMachine();
+	VendingMachine(short max_slot);
+	void addSlot(SnackSlot* slot);
+	short getEmptySlotsCount();
+private:
+	short slot_Count;
+	SnackSlot* slot_masiv_caunt = nullptr;
+};
+
